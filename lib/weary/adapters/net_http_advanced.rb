3ea94cd08953a819ffb54f77dfeb2f98c1b921f9
Adam Crownoble <adam.crownoble@biola.edu>
2014-07-10 08:52:25 -0700

    Doh!

--- lib/weary/adapters/net_http_advanced.rb
+++ lib/weary/adapters/net_http_advanced.rb
@@ -7,7 +7,6 @@ module Weary
 
       def self.connect(request)
         connection = socket(request)
-binding.pry
         connection.read_timeout = timeout unless timeout.nil?
         response = connection.request prepare(request)
         Rack::Response.new response.body || "", response.code, normalize_response(response.to_hash)
