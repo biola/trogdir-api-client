5c9b5095495b8c126e39c7393c598d92553f469b
Adam Crownoble <adam.crownoble@biola.edu>
2014-12-09 09:30:14 -0800

    Support specifying port in Trogdir URL

--- lib/trogdir_api_client/configuration.rb
+++ lib/trogdir_api_client/configuration.rb
@@ -7,18 +7,20 @@ module TrogdirAPIClient
 
     attr_accessor :scheme
     attr_accessor :host
+    attr_accessor :port
     attr_accessor :script_name
     attr_accessor :version
 
     def initialize
       @scheme = 'https'
       @host = 'api.biola.edu'
+      @port = nil
       @script_name = 'directory'
       @version = 'v1'
     end
 
     def base_url
-      URI.join(root_url, "/#{script_name}/", version).to_s
+      URI.join(root_url.to_s, "/#{script_name}/", version).to_s
     end
 
     def credentials
@@ -28,7 +30,7 @@ module TrogdirAPIClient
     private
 
     def root_url
-      URI::Generic.build(scheme: scheme, host: host)
+      URI::Generic.build(scheme: scheme, host: host, port: port)
     end
   end
 end
